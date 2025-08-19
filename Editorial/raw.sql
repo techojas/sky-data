f1d7d3ddea30b536ec672d0f2b9cbe70e9c8c9fe
ojas <ojaswini.sethi@sky.uk>
2025-08-19 16:50:49 +0100

    making changes

--- Editorial/raw.sql
+++ Editorial/raw.sql
@@ -1,17 +1,17 @@
-SELECT visitor_id
-      ,session_id
-      ,tracking_id 
-      ,page_name 
-      ,rail_name
-      ,show_title
-      ,tile_col
-      ,tile_row
-      ,adobe_date
-      ,subsection
+SELECT visitor_id;
+      ,session_id;
+      ,tracking_id; 
+      ,page_name ;
+      ,rail_name;
+      ,show_title;
+      ,tile_col;
+      ,tile_row;
+      ,adobe_date;
+      ,subsection;
       from `nowott-ds-prod-001.GDLDatamartSilver.SilverClickStreamAnalytics` 
 
-      WHERE visitor_id = 'xx12'
-      and page_name = 'Editorial'
+      WHERE visitor_id = '123412'
+      and page_name = 'Editorial';
 
 
 
